e30d6876c77552ed3ada2840007cb592ff0b13ea
Elisha <62874921+ElishaPhillips@users.noreply.github.com>
2022-02-25 20:09:26 -0800

    Update lifeGUI.java

--- src/Life/lifeGUI.java
+++ src/Life/lifeGUI.java
@@ -1,109 +1,103 @@
 package Life;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.util.*;
 
 /**
- * lifePrint class contains string formatting and user input methods
+ * Description: GUI Class for the lifeBoard
  *
- * @author Tiana Noll-Walker
- * @author Elisha Phillips
  */
-public class lifePrint {
+public class lifeGUI {
+    // Declaring the Window
+    private javax.swing.JFrame win;
 
-    // Default characters for cell states
-    private char dead = '.';
-    private char live = 'O';
-    private lifeGUI display = new lifeGUI();
-    private Buttons btn = new Buttons();
+    // Declaring the Content Pane
+    static java.awt.Container c;
 
-    /**
-     * Obtains user input value and rejects any input
-     * not meeting expected int return > minimum
-     *
-     * @param minimum integer value for lower limit, user input must be greater than
-     *
-     * @return num integer value for user input
-     */
-    public int input(int minimum) {
+    static ArrayList<Label> label = new ArrayList<Label>();
 
-        // Instantiates scanner object
-        Scanner scan = new Scanner( System.in);
+    // Background color for the layout, will form the board's gridlines
+    private static final Color live = Color.black;
+    private static final Color dead = Color.white;
 
-        // Stores integer input
-        int num ;
 
-        // Infinite loop
-        while (true) {
-            // Catch input exceptions
-            try
-            {
-                // Trying integer input
-                num = scan.nextInt();
-                // Also throw exception if it isn't greater than limit
-                if (num < minimum) {
-                    throw new InputMismatchException();
-                } else {
-                    break;
-                }
-            }
-            catch (InputMismatchException exception)
-            {
-                // User error message
-                System.out.println("\nPlease enter a valid integer greater than " + minimum + ": \n");
-                // Begin next input
-                scan.next();
-            }
+    public lifeGUI() {
+        // Main Window
+        win = new javax.swing.JFrame("TicTacToe");
+        // Content Panel
+        c = win.getContentPane();
+        c.setLayout(new BorderLayout());
+        // c.add(Buttons.toolbar, BorderLayout.NORTH);
 
-        }
-        return num;
+        // Assigning default window parameters
+        win.setLocation(50, 50);
+        win.setSize(1000, 1000);
+        win.setDefaultCloseOperation(javax.swing.JFrame.DISPOSE_ON_CLOSE);
+        win.setVisible(true);
+        win.toFront();
+
     }
 
     /**
-     * Overrides toString method, inputs square integer array and
-     * generates formatted string with default characters for
-     * cell states.
-     *
-     * @param grid integer array representing game board,
-     *             values are either 1 for alive, or 0 for dead
-     *
-     * @return boardStr formatted string
+     * Constructor for layout objects
      */
-    public String toString(int[][] grid) {
-
-        // Empty string
-        String boardStr="";
-
-        // Loop through inputted grid
-        for (int i=0;i<grid.length-1;i++) {
-            for(int j=0;j<grid[i].length-1;j++) {
-                // Check grid cell for live or dead state and update string
-                if (grid[i][j] == 0) {
-                    boardStr += dead + " ";
-                } else {
-                    boardStr += live + " ";
-                }
-            }
-            // Next line after row is complete
-            boardStr += "\n";
-        }
-        return boardStr;
-    }
-    public void GUI(ArrayList<int[][]> gridList) {
-        //Draw window
-        display.drawGridLayout(gridList.get(0));
-        btn.retrieveGrid(gridList);
-
-
-    }
-    public static void wait(int ms)
+    public void drawGridLayout(int[][] gridArr)
     {
-        try
-        {
-            Thread.sleep(ms);
+        int x = gridArr.length;
+
+        JInternalFrame grid = new JInternalFrame();
+        grid.pack();
+        grid.setVisible(true);
+
+        grid.setBackground(live);
+        grid.setLayout(new GridBagLayout());
+
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.weightx = 1d;
+        gc.weighty = 1d;
+        gc.insets = new Insets(0, 0, 1, 1);
+        gc.fill = GridBagConstraints.BOTH;
+
+        int index = 0;
+        // fill the whole panel with labels
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                Label l = new Label();
+                l.setBackground(dead);
+                gc.gridx = i;
+                gc.gridy = j;
+                grid.add(l, gc);
+                label.add(index, l);
+                index += 1;
+            }
         }
-        catch(InterruptedException ex)
-        {
-            Thread.currentThread().interrupt();
+
+        updateGridLayout(gridArr);
+        c.add(grid, BorderLayout.CENTER);
+
+    }
+
+
+
+    public void updateGridLayout(int[][] gridArr) {
+
+        int x = gridArr.length;
+        int index;
+        for (int i=0;i<x;i++) {
+            for(int j=0;j<x;j++) {
+                index = (x*j) + i;
+                // Check grid cell for live or dead state and update string
+                if (gridArr[i][j] == 0) {
+                    label.get(index).setBackground(live);
+                } else {
+                    label.get(index).setBackground(dead);
+                }
+                index += 1;
+            }
         }
     }
+
+
 }
