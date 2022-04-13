f9c068736fab69a076fdb286aa7d7cce822b5d02
Elisha <62874921+ElishaPhillips@users.noreply.github.com>
2022-04-13 09:35:05 -0700

    Add files via upload

---
+++ src/Life/tblabelStyle.java
@@ -0,0 +1,29 @@
+package Life;
+
+import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import java.awt.*;
+
+public class tblabelStyle extends JLabel {
+
+    private Border line;
+    private Border margin;
+    private Border compound;
+    private JPanel panel;
+    static Font font = new Font("SANS_SERIF", Font.PLAIN, 12);
+
+    public tblabelStyle() {
+        line = BorderFactory.createLineBorder(Color.BLACK);
+        margin = BorderFactory.createEmptyBorder(20, 20, 20, 20);
+        compound = new CompoundBorder(line, margin);
+        this.setForeground(Color.BLACK);
+        this.setBackground(Color.WHITE);
+        this.setBorder(compound);
+        this.setPreferredSize(new Dimension(50, 40));
+        this.setFont(font);
+
+    }
+
+}
